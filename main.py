bd64867500680d7a458029a3f565dc567cc764e7
huewhg <153721674+huewhg@users.noreply.github.com>
2025-04-11 15:11:01 +0200

    Update and rename joy.py to main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from microbit import *
+import time
+def joystickGetXY(pinX, pinY, screen):
+
+    #pin2.set_pull(pin2.PULL_UP)
+    #divide it to 5*5 range
+    segmentSize = 1024 /5
+
+    #X axes
+    coordsX = pinX.read_analog()
+    if screen:
+        coordsX = int(coordsX / segmentSize)
+
+    #Y axes
+    coordsY = pinY.read_analog()
+    if screen:
+        coordsY = int(coordsY / segmentSize)
+    return coordsX, coordsY
+
+def driveServo(pin, num):
+    if num > 1024 or num < 0:
+        raise ValueError("Input value must be between 0 and 1024.")
+    new_value = 8 + num * (134 - 8) / 1024
+    pin.write_analog(new_value)
+    return True
+while True:
+    x, y = joystickGetXY(pin0, pin1, False)
+    driveServo(pin8, y)
+    x, y = joystickGetXY(pin0, pin1, True)
+    display.set_pixel(x, y, 9)
+    #sleep(1)
+    display.clear()
+
