c48036f5a68a0923f6febadca822975fecaacd27
Korgie <159555300+SUPERPRDITKO@users.noreply.github.com>
2025-04-12 01:41:56 +0200

    Update easymb.py

--- easymb.py
+++ easymb.py
@@ -142,5 +142,50 @@ class segment:
                     self._update()
                     sleep(5)
                     t += 5
+    class setup:
+        def controlPasswordSet():
+            """ pause the cont. and search for password in all channels"""
+            radio.on()
+            password = 0
+            chan = 0
+            while True:
+                display.show(password)
+                chan += 1
+                try: radio.config(channel= chan)
+                except: chan = 0
+                    
+                if button_b.was_pressed():
+                    password += 1
+                    if (password == 10):
+                        password = 0
+                if (radio.receive() == str(password)):
+                    display.scroll("y")
+                    for i in range(100):
+                        radio.send(str(password))
+                    break
+        
+        def carSet():
+            """ pause the car and await the signal matching the password """
+            radio.on()
+            password = 0
+            chan = 1
+            while True:
+                display.show(password)
+                sleep(400)
+                display.show(chan)
+                sleep(400)
+                display.show(Image.HEART)
+                sleep(800)
+                if button_a.was_pressed():
+        
+                    password += 1
+                elif button_b.was_pressed():
+                    chan += 1
+                    radio.config(channel= chan)
+                    
+                radio.send(str(password))
+                if (radio.receive() == str(password)):
+                    display.scroll("y")
+                    break
 
 
